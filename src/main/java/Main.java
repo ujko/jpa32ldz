31e317162ab52fc3fa50fa655600e213c37736dd
uniko <uio6398@gmail.com>
2021-04-18 09:40:50 +0200

    jar runnable

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,18 +4,23 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("test");
         SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
         Session session = factory.openSession();
-
-        Query<Employee> employeeQuery = session
-                .createNamedQuery("getByFirstName", Employee.class)
-                .setParameter("firstName", "Adolf");
-        System.out.println(employeeQuery.getResultList());
-
+        Arrays.stream(args).forEach(x -> showData(session, x));
         session.close();
         factory.close();
+    }
+
+    private static void showData(Session session, String name) {
+        Query<Employee> employeeQuery = session
+                .createNamedQuery("getByFirstName", Employee.class)
+                .setParameter("firstName", name);
+        System.out.println(employeeQuery.getResultList());
     }
 }
